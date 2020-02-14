ae59fc7f424a65d5c0aa1897ce6fad71dbf031c1
Leonardo Oliveira Gonçalves <oleonardo.goncalves@gmail.com>
2020-02-14 12:16:27 -0300

    Initial Commit

---
+++ Alcool ou gasolina/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alcool ou gasolina
+//
+//  Created by Leonardo Oliveira on 14/02/20.
+//  Copyright © 2020 Leonardo Gonçalves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
